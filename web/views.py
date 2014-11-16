108cb281d97552319216397c1e6c22da31357d04
Joseph Toles <jbtoles@gmail.com>
2014-11-15 23:34:06 -0800

    Removed print statements

--- web/views.py
+++ web/views.py
@@ -6,20 +6,14 @@ from django.template import RequestContext, loader
 
 
 def home_view(request):
-    print 'getting home view'
     context = {}
     if request.method == 'POST':
-        print 'request is POST'
         form = URLForm(request.POST)
         if form.is_valid():
-            print 'form is valid'
             # set search url here
             url = form.cleaned_data['url']
             context['root'] = url
-            print 'url is %s' % url
-        print 'form is not valid'
     else:
-        print 'method is not POST'
         form = URLForm() # An unbound form
     context['form'] = form
     return render_to_response('home.html', context, RequestContext(request))
