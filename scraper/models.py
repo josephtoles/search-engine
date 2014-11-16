8c9482f1410e2c6510a48e2c2bbc044a10185b14
Joseph Toles <jbtoles@gmail.com>
2014-11-15 23:04:26 -0800

    Added Website and Webpage models

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Website(models.Model):
+    url = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # Robots.txt
+    robots_html = models.TextField()
+    robots_updated = models.DateTimeField()
+    robots_get_count = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['url']
+
+
+class Webpage(models.Model):
+    # field: foreign key to Website
+    website = models.ForeignKey('Website')
+    url = models.URLField()  # Full URL including website's base URL
+
+    # whether this page exists
+    exists = models.BooleanField()
+
+    # HTML content of Webpage
+    content = models.TextField()
+
+    # number of times this page has been crawled
+    crawl_count = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
