5f8eaef670166ca55adaea8458cd0be605b05ecd
Joseph Toles <jbtoles@gmail.com>
2014-11-16 20:01:17 -0800

    Added models to crawler

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+def Website(models.Model):
+    url = models.URLField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # robots.txt
+    robots_exists = models.BooleanField()
+    robots_content = models.TextField()
+    robots_updated = models.DateTimeField()
+
+    class Meta:
+        ordering = ['url']
+    
+
+def Webpage(models.Model):
+    url = models.URLField(unique=True)
+    robots_allowed = models.BooleanField()
+    website = models.ForeignKey('Website')
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['url']
+
