5f032db3bd57bbde6e45791b1c6070f1e0477e36
Joseph Toles <jbtoles@gmail.com>
2014-11-21 15:50:35 -0800

    Added force option to crawler

--- web/crawler.py
+++ web/crawler.py
@@ -45,6 +45,7 @@ def update_robots_txt_if_necessary(website):
     print 'handling robots.txt'
     if not robots_txt_updated_recently(website):
         robots_url = urljoin('http://' + website.url, 'robots.txt')
+        print 'robots_url is %s' % robots_url
         response = urllib2.urlopen(robots_url)
         html = response.read()
         website.robots_content = html
@@ -58,17 +59,17 @@ def crawled_recently(webpage):
 # returns a tuple (webpage, created)
 # where webpage is the webpage (None if not accessible)
 # and created is a boolean representing whether the webpage was actually fetched with this call
-def crawl_url(url):
-    print 'crawling url'
-    base_url = urlparse(url).netloc
-    website, created = Website.objects.get_or_create(url=base_url)
+def crawl_url(url, website, force=False):
     update_robots_txt_if_necessary(website)
     rerp = RobotExclusionRulesParser()
     rerp.parse(website.robots_content)
     if rerp.is_allowed('*', '/foo.html'):
         webpage, created = Webpage.objects.get_or_create(url=url, website=website)
         # update webpage content
-        if created or not crawled_recently(webpage):
+        if created or force or not crawled_recently(webpage):
+            print 'opening url %s' % url
+            if url.startswith('/'):  # TODO clean up this hack
+                url = urljoin('http://' + website.url, url)
             response = urllib2.urlopen(url)
             html = response.read()
             #webpage.content = str(html)  # 8-bit to unicode
@@ -83,29 +84,46 @@ def crawl_url(url):
         return (None, False)
 
 # get links from a block of html
-def get_links(html):
-    #TODO this doesn't work. Fix it.
-    print 'getting links'
+def get_links(html, website):
+    urls = []
     soup = BeautifulSoup(html)
-    output = soup.find_all('a')
-    print output
-    return output
+    links = soup.find_all('a')
+
+    for tag in links:
+        link = tag.get('href',None)
+        if link != None:
+            urls.append(link)
+
+    # remove external links to other websites
+    indices = range(0, len(urls))
+    indices.reverse()
+    for i in indices:
+        netloc = urlparse(urls[i]).netloc
+        if netloc and netloc != website.url:
+            del(urls[i])
+
+    # TODO 
+    return urls
 
 # breadth-first recusive url search
 # input a domain and then get that and all subdomains
 # when first called, set base_url = current_url
-def crawl_url_subdomains(base_url, num_left=20):
+def crawl_url_subdomains(url, num_left=20):
+    base_url = urlparse(url).netloc
+    website, created = Website.objects.get_or_create(url=base_url)
+    if not base_url:
+        raise ValueError('base_url cannot be blank')
     print 'crawling url subdomains'
-    links = [str(base_url)]
+    links = [str(url)]
     i = 0
     while(i <= len(links)):
         print 'crawling recursive, i=%s' % i
-        webpage, updated = crawl_url(links[i])
+        webpage, updated = crawl_url(links[i], website, i==0)
         if updated:
             # TODO add sleep command here
             num_left -= 1
         if webpage:
             html = webpage.content
-            links.extend(get_links(html))
+            links.extend(get_links(html, webpage.website))
         i += 1
 
