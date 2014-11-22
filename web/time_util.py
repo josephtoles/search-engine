aa0fc1da00acd7e6c201788f1001c51723152c41
Joseph Toles <jbtoles@gmail.com>
2014-11-21 22:36:01 -0800

    Added several files

---
+++ web/time_util.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta, tzinfo
+
+#####################
+# TIMEZONE HANDLING #
+#####################
+
+ZERO = timedelta(0)
+class UTC(tzinfo):
+  def utcoffset(self, dt):
+    return ZERO
+  def tzname(self, dt):
+    return "UTC"
+  def dst(self, dt):
+    return ZERO
+
