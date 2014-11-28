34009702bf1e1fd97bf6f2e1405130b3694a16e3
Joseph Toles <jbtoles@gmail.com>
2014-11-28 12:41:41 -0800

    Added test code to manage.py

--- manage.py
+++ manage.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    execute_from_command_line('pip install requirements.txt')
     execute_from_command_line(sys.argv)
