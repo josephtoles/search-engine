8bb6b8389d57f33664dfb7045ab5504ccfdf2c3d
Joseph Toles <jbtoles@gmail.com>
2014-11-28 12:43:34 -0800

    Removed pip install from manage.py

--- manage.py
+++ manage.py
@@ -7,5 +7,4 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line('pip install requirements.txt')
     execute_from_command_line(sys.argv)
