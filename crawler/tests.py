cd69581b5661b0524aa3b18e6054c48df3e4ee51
Joseph Toles <jbtoles@gmail.com>
2014-12-06 14:25:55 -0800

    Added blank unit test

--- crawler/tests.py
+++ crawler/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from utils.testcase import TestCase
+from models import Website
 
-# Create your tests here.
+class CrawlerTest(TestCase):
+
+    def test_create_website_model(self):
+        Website.objects.create(url='Amazon.com')
+        self.assertEqual(Website.objects.count(), 1)
+
+    def test_url_cannot_be_blank(self):
+        Website.objects.create(url='')
+        self.assertEqual(Website.objects.count(), 0)
+
