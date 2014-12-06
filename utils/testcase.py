c9325fd2aac6826dbb40c3e07b4cca815a9accc1
Joseph Toles <jbtoles@gmail.com>
2014-12-06 13:27:53 -0800

    Added back utils functions

---
+++ utils/testcase.py
@@ -0,0 +1,21 @@
+# Includes helper code for testing
+from django.test import TestCase as BaseTestCase
+from django.contrib.auth.models import User
+
+
+DEFAULT_USER = {
+        'username': 'Lisbeth Salander',
+        'email': 'wasp@anonymous.ru',
+        'password': 'password',}
+
+
+# Overloads default Django TestCase to provide useful features
+class TestCase(BaseTestCase):
+
+    def setUp(self):
+        # Create a default user
+        self.user = User.objects.create_user(
+            username = DEFAULT_USER['username'],
+            email    = DEFAULT_USER['email'],
+            password = DEFAULT_USER['password'],)
+
