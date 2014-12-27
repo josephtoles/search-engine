c77e5b50375ed766af7c2f726d3eab8b1295665c
Joseph Toles <jbtoles@gmail.com>
2014-12-26 19:22:09 -0800

    Added skeleton unit test

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
 
 class APITest(TestCase):
 
     def test_api(self):
-        print 'testing'
+        url = reverse('search-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        self.assertEqual(response.content, '[]')
